2d553a71125bf571bb31922af56123f4375bdbfa
haz0110 <hazar@a0110.com>
2023-05-28 21:33:36 +0300

    p9

--- src/euler_specific/mod.rs
+++ src/euler_specific/mod.rs
@@ -1,1 +1,2 @@
 pub mod p5_smallest_multiple;
+pub mod p9_special_pythagorean_triplet;
---
+++ src/euler_specific/p9_special_pythagorean_triplet.rs
@@ -0,0 +1,29 @@
+pub fn special_pythagorean_triplet() -> usize {
+
+    let mut vector: Vec<usize> = Vec::new();
+
+    for number_a in 1..1000 as usize {
+        for number_b in 1..1000 {
+            for number_c in 1..1000 {
+                if 
+                number_a < number_b && 
+                number_b < number_c &&
+                number_a.pow(2) + number_b.pow(2) == number_c.pow(2) &&
+                number_a + number_b + number_c == 1000 {
+                    vector = vec![number_a, number_b, number_c];
+                }
+            }
+        }
+    }
+    vector[0] * vector[1] * vector[2]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn problem9_test() {
+        assert_eq!(special_pythagorean_triplet(), 31875000);
+    }
+}
