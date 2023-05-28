ed9c5abd06e62a103d4a05ca2d65457b9506cf7a
haz0110 <hazar@a0110.com>
2023-05-28 10:25:40 +0300

    palindromes optimized

--- src/mathematics/palindrome.rs
+++ src/mathematics/palindrome.rs
@@ -1,45 +1,40 @@
-pub fn palindromes_2_digits() -> Vec<usize> {
-    let mut storage: Vec<usize> = Vec::with_capacity(1000);
-    for number in (10 * 10)..(99 * 99) {
-        let number_as_string: String = format!("{:?}", number);
-        let reversed_number_as_string = number_as_string.chars().rev().collect::<String>();
-        let reversed_number: usize = reversed_number_as_string.parse().unwrap();
+use palindrome::*;
 
-        if number == reversed_number {
-            'inLoop: for first_multiplier in 10..=99 {
-                for second_multiplier in 10..=99 {
-                    if second_multiplier * first_multiplier == number {
-                        println!("{} * {} = {}", first_multiplier, second_multiplier, number);
-                        storage.push(number);
-                        break 'inLoop;
-                    }
-                }
-            }
-        }
-    }
-
-    storage
+pub fn is_palindrome_number(number: usize) -> bool {
+    let number_as_string: String = format!("{:?}", number);
+    is_palindrome(number_as_string)
 }
 
-pub fn palindromes_3_digits() -> Vec<usize> {
+pub fn palindromes(digit: usize) -> Vec<usize> {
     let mut storage: Vec<usize> = Vec::with_capacity(1000);
-    for number in (100 * 100)..(999 * 999) {
-        let number_as_string: String = format!("{:?}", number);
-        let reversed_number_as_string = number_as_string.chars().rev().collect::<String>();
-        let reversed_number: usize = reversed_number_as_string.parse().unwrap();
 
-        if number == reversed_number {
-            'inLoop: for first_multiplier in 100..=999 {
-                for second_multiplier in 100..=999 {
-                    if second_multiplier * first_multiplier == number {
-                        storage.push(number);
-                        break 'inLoop;
+    match digit {
+        2 => for number in (10 * 10)..(99 * 99) {
+            if is_palindrome(number) {
+                'inLoop: for first_multiplier in 10..=99 {
+                    for second_multiplier in 10..=99 {
+                        if second_multiplier * first_multiplier == number {
+                            storage.push(number);
+                            break 'inLoop;
+                        }
                     }
                 }
             }
-        }
+        },
+        3 => for number in (100 * 100)..(999 * 999) {
+            if is_palindrome(number) {
+                'inLoop: for first_multiplier in 100..=999 {
+                    for second_multiplier in 100..=999 {
+                        if second_multiplier * first_multiplier == number {
+                            storage.push(number);
+                            break 'inLoop;
+                        }
+                    }
+                }
+            }
+        },
+        _ => return vec![0]
     }
-
     storage
 }
 
@@ -48,14 +43,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn palindromes_2_digits_test() {
-        assert!(palindromes_2_digits()[0] == 121);
-        assert!(palindromes_2_digits()[8] == 444);
-    }
-
-    #[test]
-    fn palindromes_3_digits_test() {
-        assert_eq!(palindromes_3_digits()[0], 10_201);
-        assert_eq!(palindromes_3_digits()[1], 11_211);
+    fn palindromes_test() {
+        assert_eq!(palindromes(2)[0], 121);
+        assert_eq!(palindromes(2)[8], 444);
+        assert_eq!(palindromes(3)[0], 10_201);
+        assert_eq!(palindromes(3)[1], 11_211);
     }
 }
