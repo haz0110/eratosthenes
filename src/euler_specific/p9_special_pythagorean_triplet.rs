4d7bba7db902912b12f4fc6e90eeb8dd41a6b0aa
haz0110 <hazar@a0110.com>
2023-05-30 12:16:26 +0300

    .

--- src/euler_specific/p9_special_pythagorean_triplet.rs
+++ src/euler_specific/p9_special_pythagorean_triplet.rs
@@ -2,13 +2,13 @@ pub fn special_pythagorean_triplet() -> usize {
 
     let mut vector: Vec<usize> = Vec::new();
 
-    for number_a in 1..1000 as usize {
+    for number_a in 1..1000 {
         for number_b in 1..1000 {
             for number_c in 1..1000 {
                 if 
                 number_a < number_b && 
                 number_b < number_c &&
-                number_a.pow(2) + number_b.pow(2) == number_c.pow(2) &&
+                (number_a as usize).pow(2) + (number_b as usize).pow(2) == (number_c as usize).pow(2) &&
                 number_a + number_b + number_c == 1000 {
                     vector = vec![number_a, number_b, number_c];
                 }
